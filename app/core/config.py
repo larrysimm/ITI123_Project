11297286a9bd34030c913a36b1b245794b342014
Larry <sim.larry88@gmail.com>
2026-01-18 20:38:10 +0800

    update config

--- app/core/config.py
+++ app/core/config.py
@@ -37,5 +37,9 @@ class Settings:
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+
+    # --- PINECONE CONFIG ---
+    PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME")
 
 settings = Settings()
